5bc7f8c6764241999ea134339c4295a89788a907
ted555 <thanong984@gmail.com>
2019-03-15 16:04:43 +0700

    Init commit

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:LearningDart/LearningDart.dart' as LearningDart;
+
+main(List<String> arguments) {
+  print("SampleCalculator: " + LearningDart.calculate().toString());
+}
---
+++ test/LearningDart_test.dart
@@ -0,0 +1,10 @@
+import 'package:LearningDart/LearningDart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+
+}
+
